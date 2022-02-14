aee94bde0f113b782d55d32541e401eab06dffdc
Dynesshely <2369913161@qq.com>
2022-02-14 22:22:31 +0800

    🎢 import lib

---
+++ oth-win-control/oth-win-control/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace oth_win_control
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Find (the first-in-Z-order) Notepad window.
+            IntPtr hWnd = FindWindow("Notepad", null);
+
+            // If found, position it.
+            if (hWnd != IntPtr.Zero)
+            {
+                // Move the window to (0,0) without changing its size or position
+                // in the Z order.
+                SetWindowPos(hWnd, IntPtr.Zero, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
+            }
+        }
+
+        // P/Invoke declarations.
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        const uint SWP_NOSIZE = 0x0001;
+        const uint SWP_NOZORDER = 0x0004;
+    }
+}
